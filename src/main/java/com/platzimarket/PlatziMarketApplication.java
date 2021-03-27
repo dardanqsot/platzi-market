2bb52073296b5efb602798dae518ca53d44f8c7b
Dardan <quispesotodaniel@gmail.com>
2021-03-27 00:49:16 -0500

    Origen de proyecto

--- src/main/java/com/platzimarket/PlatziMarketApplication.java
+++ src/main/java/com/platzimarket/PlatziMarketApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PlatziMarketApplication {
-	//comentario de prueba para GIT
+	//comentario de prueba para GIT P2
 	public static void main(String[] args) {
 		SpringApplication.run(PlatziMarketApplication.class, args);
 	}
